65aa8a3f188b0cc497f7d286666eb1b69974e7ef
Tiago de Freitas <tiagoabc@gmail.com>
2022-10-02 12:16:35 -0300

    Update 16.py

--- Lista 3 - 06-10-22/16.py
+++ Lista 3 - 06-10-22/16.py
@@ -10,28 +10,35 @@
 #Data atual: 30-Set-22
 
 import math
+import os
+import sys
 
 #Leitura de Variáveis
-print('Exercício 15 - Três lados de um triângulo\n')
+print('Exercício 16 - Equação do segundo grau\n')
 a=float(input('Digite o valor de a: '))
 b=float(input('Digite o valor de b: '))
 c=float(input('Digite o valor de c: '))
 
 #Lógica cálculos
 
-if a==0:
-    print('\nNão é uma equação do 2° grau')
+if a==0:    
+    try:
+        sys.exit('')
+    except SystemExit:
+        print('\nNão é uma equação do 2° grau')
+        sys.exit()
 else:
     delta = (b ** 2) - (4 * a * c)
     
 if delta < 0:
     print('\nDelta:',delta)
-    print('O delta é negativo. Equação não possui raízes reais')
+    print('Como o Delta é negativo, a equação não possui raízes reais')
 else:
     x1 = (-b + math.sqrt(delta)) / 2 * a
     x2 = (-b - math.sqrt(delta)) / 2 * a
 
 if delta == 0:
-    print('1 - Raiz real:', x1)
+    print('\nRaiz real:', x1)
 else:
-    print('2 - Raizes reais: ', '\nx1: ',x1, '\nx2: ',x2)
+    print('\nDelta:', delta)
+    print('Raizes reais: ', '\nx1: ',x1, '\nx2: ',x2)
